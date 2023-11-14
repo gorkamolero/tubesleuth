f5c70102f1b93527747d966a3af36e49c7f6c1fb
Gorka <gorka.molero@gmail.com>
2023-11-14 16:41:54 +0000

    First test

--- src/main.js
+++ src/main.js
@@ -1,33 +1,60 @@
-import { Client } from 'node-appwrite';
+import { Client, Storage } from "node-appwrite"
+import { v4 as uuidv4 } from "uuid"
+import OpenAI from "openai"
+import fs from "fs"
+import path from "path"
 
-// This is your Appwrite function
-// It's executed each time we get a request
+// Initialize OpenAI client
+const openai = new OpenAI()
+
 export default async ({ req, res, log, error }) => {
-  // Why not try the Appwrite SDK?
-  //
-  // const client = new Client()
-  //    .setEndpoint('https://cloud.appwrite.io/v1')
-  //    .setProject(process.env.APPWRITE_FUNCTION_PROJECT_ID)
-  //    .setKey(process.env.APPWRITE_API_KEY);
+  // Initialize Appwrite Client
+  const client = new Client()
+    .setEndpoint("https://cloud.appwrite.io/v1") // Replace with your Appwrite endpoint
+    .setProject(process.env.APPWRITE_FUNCTION_PROJECT_ID)
+    .setKey(process.env.APPWRITE_API_KEY)
 
-  // You can log messages to the console
-  log('Hello, Logs!');
+  const storage = new Storage(client)
 
-  // If something goes wrong, log an error
-  error('Hello, Errors!');
+  if (req.method === "POST") {
+    try {
+      // Assuming the script is sent in the request body
+      const script = req.payload.script
 
-  // The `req` object contains the request data
-  if (req.method === 'GET') {
-    // Send a response with the res object helpers
-    // `res.send()` dispatches a string back to the client
-    return res.send('Hello, World!');
+      // Perform text-to-speech conversion
+      const mp3Response = await openai.audio.speech.create({
+        model: "tts-1",
+        voice: "onyx",
+        input: script,
+      })
+
+      // Temporary file to store the MP3
+      const tempFile = path.resolve("/tmp/" + uuidv4() + ".mp3")
+      const buffer = Buffer.from(await mp3Response.arrayBuffer())
+      await fs.promises.writeFile(tempFile, buffer)
+
+      // Create a file in Appwrite storage
+      const result = await storage.createFile(
+        process.env.APPWRITE_BUCKET_ID,
+        uuidv4(), // Generating a unique ID for the file
+        fs.createReadStream(tempFile)
+      )
+
+      // Clean up the temporary file
+      fs.unlinkSync(tempFile)
+
+      // Return the response with file ID or URL, with status
+      return res.json({
+        status: "success",
+        file: result.$id,
+        url: result.$url,
+      })
+    } catch (error) {
+      error("Error in text-to-speech conversion or file upload:", error)
+      return res.json({ error: error.message })
+    }
   }
 
-  // `res.json()` is a handy helper for sending JSON
-  return res.json({
-    motto: 'Build like a team of hundreds_',
-    learn: 'https://appwrite.io/docs',
-    connect: 'https://appwrite.io/discord',
-    getInspired: 'https://builtwith.appwrite.io',
-  });
-};
+  // Default response for non-POST requests
+  return res.send("Please use POST request with a script.")
+}
